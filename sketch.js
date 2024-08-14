c5620966d36fac6328fbacf29b68de82d2fd53b5
Laura <00001132654841sp@aluno.educacao.sp.gov.br>
2024-08-14 10:53:54 -0300

    adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(600, 600);
+    background("black");
+  }
+  
+  function draw() {
+    background(220);
+    stroke("blue");
+    fill("red");
+    
+    if(mouseIsPressed) {
+       rect(mouseX, mouseY, 20, 35);
+    }
+    
+  }
